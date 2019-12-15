6bb94d65152ee8e6f15b9e83d2a33f2056c2dff8
Kazi Jamal <jamalkazi02@gmail.com>
2019-12-15 00:40:39 -0500

    updated error handling

--- app.js
+++ app.js
@@ -56,8 +56,9 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
 	res.status(error.status || 500);
 	res.json({
+		message: 'Error',
 		error: {
-			message: error.message
+			errorMessage: error.message
 		}
 	});
 });
