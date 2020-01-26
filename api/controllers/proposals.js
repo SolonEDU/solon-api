4149216f409f586d964a9363786a7a5cc4f4d4d8
ray. lee <rlee00@stuy.edu>
2020-01-26 15:42:27 -0500

    added error handling

--- api/controllers/proposals.js
+++ api/controllers/proposals.js
@@ -1,39 +1,39 @@
-require('dotenv').config();
-const db = require('../../config/database');
-const Proposal = require('../models/Proposal');
-var googleTranslate = require('google-translate')(process.env.GOOGLE_CLOUD_KEY);
+require("dotenv").config();
+const db = require("../../config/database");
+const Proposal = require("../models/Proposal");
+var googleTranslate = require("google-translate")(process.env.GOOGLE_CLOUD_KEY);
 
 const translate = async (text, target) => {
-    return new Promise((resolve, reject) => {
-        googleTranslate.translate(text, target, (err, res) => {
-            if (err) reject(err);
-            else resolve(res.translatedText);
-        });
+  return new Promise((resolve, reject) => {
+    googleTranslate.translate(text, target, (err, res) => {
+      if (err) reject(err);
+      else resolve(res.translatedText);
     });
-}
+  });
+};
 
-const toTranslationMap = async (text) => {
-    var translations = new Object();
-    const langCodeMap = {
-        'en': 'en',
-        'zhcn': 'zh-CN',
-        'zhtw': 'zh-TW',
-        'bn': 'bn',
-        'ko': 'ko',
-        'ru': 'ru',
-        'ja': 'ja',
-        'uk': 'uk'
-    };
-    for (var key in langCodeMap) {
-        const translation = await translate(text, langCodeMap[key])
-        translations[key] = translation;
-        console.log(translations[key]);
-    }
-    return JSON.stringify(translations);
-}
+const toTranslationMap = async text => {
+  var translations = new Object();
+  const langCodeMap = {
+    en: "en",
+    zhcn: "zh-CN",
+    zhtw: "zh-TW",
+    bn: "bn",
+    ko: "ko",
+    ru: "ru",
+    ja: "ja",
+    uk: "uk"
+  };
+  for (var key in langCodeMap) {
+    const translation = await translate(text, langCodeMap[key]);
+    translations[key] = translation;
+    console.log(translations[key]);
+  }
+  return JSON.stringify(translations);
+};
 
-toTranslationMap('hello').then((translated) => {
-    console.log(translated);
+toTranslationMap("hello").then(translated => {
+  console.log(translated);
 });
 
 // translate = source => {
@@ -89,66 +89,72 @@ toTranslationMap('hello').then((translated) => {
 // };
 
 exports.proposals_get_all = (req, res, next) => {
-    Proposal.findAll({
-        order: [['starttime', 'DESC']]
-    }).then(proposals => {
-        res.status(200).json({
-            message: 'All proposals were fetched',
-            proposals: proposals
-        });
+  Proposal.findAll({
+    order: [["starttime", "DESC"]]
+  }).then(proposals => {
+    res.status(200).json({
+      message: "All proposals were fetched",
+      proposals: proposals
     });
+  });
 };
 
 exports.proposals_get_proposal = (req, res, next) => {
-    const proposalID = req.params.proposalID;
-    Proposal.findOne({
-        where: {
-            pid: proposalID
+  const proposalID = req.params.proposalID;
+  Proposal.findOne({
+    where: {
+      pid: proposalID
+    }
+  }).then(proposal => {
+    if (proposal == null) {
+      res.json({
+        message: "Error",
+        error: {
+          errorMessage: `No proposal with proposalID ${proposalID}`
         }
-    }).then(proposal => {
-        if (proposal == null) {
-            res.json({
-                message: 'Error',
-                error: {
-                    errorMessage: `No proposal with proposalID ${proposalID}`
-                }
-            });
-        } else {
-            res.status(200).json({
-                message: `Proposal with proposalID ${proposalID} was fetched`,
-                proposal: proposal
-            });
-        }
-    });
+      });
+    } else {
+      res.status(200).json({
+        message: `Proposal with proposalID ${proposalID} was fetched`,
+        proposal: proposal
+      });
+    }
+  });
 };
 
 exports.proposals_create_proposal = async (req, res, next) => {
-	const translatedTitle = await toTranslationMap(req.body.title);
-	const translatedDescription = await toTranslationMap(req.body.description);
-	console.log(translatedTitle);
-    Proposal.create({
-        title: translatedTitle,
-        description: translatedDescription,
-        starttime: req.body.starttime,
-        endtime: req.body.endtime,
-        uid: req.body.uid
-    }).then(proposal => {
-        res.status(201).json({
-            message: 'Proposal was created',
-            proposal: proposal
-        });
+  var translatedTitle, translatedDescription;
+  try {
+    translatedTitle = await toTranslationMap(req.body.title);
+    translatedDescription = await toTranslationMap(req.body.description);
+    console.log(translatedTitle);
+    console.log(translatedDescription);
+  } catch (e) {
+    console.log(e);
+  }
+  Proposal.create({
+    title: translatedTitle,
+    description: translatedDescription,
+    starttime: req.body.starttime,
+    endtime: req.body.endtime,
+    uid: req.body.uid
+  }).then(proposal => {
+    res.status(201).json({
+      message: "Proposal was created",
+      proposal: proposal
     });
+  });
 };
 
 exports.proposals_delete_proposal = (req, res, next) => {
-    const proposalID = req.params.proposalID;
-    Proposal.destroy({
-        where: {
-            pid: proposalID
-        }
-    }).then(
-        res.status(200).json({
-            message: `Proposal with proposalID ${proposalID} was deleted`
-        })
-    );
+  const proposalID = req.params.proposalID;
+  Proposal.destroy({
+    where: {
+      pid: proposalID
+    }
+  }).then(
+    res.status(200).json({
+      message: `Proposal with proposalID ${proposalID} was deleted`
+    })
+  );
 };
