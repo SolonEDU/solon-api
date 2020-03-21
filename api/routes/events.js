d0b136a7589d6a7db8de3e43dcadf06255ccd10c
Kazi Jamal <jamalkazi02@gmail.com>
2020-03-21 19:14:31 -0400

    Update events.js

--- api/routes/events.js
+++ api/routes/events.js
@@ -6,7 +6,7 @@ const EventsController = require('../controllers/events');
 
 router.get('/', checkAuth, EventsController.events_get_all);
 
-router.get('/:eventID', EventsController.events_get_event);
+router.get('/:eventID', checkAuth, EventsController.events_get_event);
 
 router.post('/', checkAuth, EventsController.events_create_event);
 
